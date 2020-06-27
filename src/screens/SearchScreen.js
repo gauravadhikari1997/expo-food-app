e6f27e83573a2f8d18411a118e83a7265b7c43b6
unknown <gaurav.15bme2023@abes.ac.in>
2020-06-27 16:38:40 +0530

    setup expo react app and SearchScreen skeleton

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({});
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>SearchScreen</Text>
+    </View>
+  );
+};
+
+export default SearchScreen;
